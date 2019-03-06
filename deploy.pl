975a692fcad55aaad595e0e8432ba7ce8580406c
Victoria Offord <vo1@sanger.ac.uk>
2019-03-06 11:24:23 +0000

    test checksum comparison for local

--- deploy.pl
+++ deploy.pl
@@ -187,7 +187,8 @@ for my $directory (@{$config_settings{general}{directories_to_build}}) {
 }
 
 if ( $ENVIRONMENT eq 'local' ) {
-
+  $remote->compare_mappings(\%original_checksums, \%revised_checksums);
+  $remote->write_logfile($config_settings{deployment}{checksums}, \%revised_checksums) unless $NO_UPDATE_CHECKSUMS;
 } else {
   $remote->compare_mappings(\%original_checksums, \%revised_checksums);
   $remote->write_logfile($config_settings{deployment}{checksums}, \%revised_checksums) unless $NO_UPDATE_CHECKSUMS;
